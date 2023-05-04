ffb3e9176ef988f2efd5e1c51f3ff7440375abdb
RyanD177 <w0477770@campus.nscc.ca>
2023-05-04 11:19:39 -0300

    fun fun

--- index.js
+++ index.js
@@ -6,32 +6,33 @@ https://openweathermap.org/img/wn/
 "this.displayWeather"
 */
 let weather = {
-   
+
     apikey: "d9a16e13727944fb091660e8020abeca",
     fetchWeather: function (city) {
- 
+
         fetch(
             "https://api.openweathermap.org/data/2.5/weather?q="
             + city
             + "&units=metric&appid="
             + this.apikey
         )
-        .then((response =>  response.json()))
-        .then((data => this.displayWeather(data)))
-        },
+            .then((response => response.json()))
+            .then((data => this.displayWeather(data)))
+    },
     displayWeather: function (data) {
-        const {name} = data;
+        const { name } = data;
+        const { icon, description } = data.weather[0];
         console.log(name)
-        document.querySelector(".city").innerHTML = `Weather in ${name}`
-       
-    
+        console.log(icon,description)
+        document.querySelector(".city").innerHTML = `Weather in ${name}`;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
 
 
 
     }
-   
-    }
+
+}
 
 
-weather.fetchWeather("Africa")
+weather.fetchWeather("Zimbabwe")
 
