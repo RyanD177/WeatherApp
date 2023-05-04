acb11327c3194335bb71e3dadfb9d138d263650c
RyanD177 <w0477770@campus.nscc.ca>
2023-05-04 10:13:04 -0300

    easy fast get weather

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-const API_KEY = "d9a16e13727944fb091660e8020abeca";
+/*"d9a16e13727944fb091660e8020abeca",
+"https://api.openweathermap.org/data/2.5/weather?q="
++ "&units=metric&appid="
+https://openweathermap.org/img/wn/
+@2x.png
+"this.displayWeather"
+*/
+let weather = {
+   
+    apikey: "d9a16e13727944fb091660e8020abeca",
+    fetchWeather: function (city) {
+    try{
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+            + city
+            + "&units=metric&appid="
+            + this.apikey
+        )
+        .then((response =>  response.json()))
+        .then((data => console.log(data)))
+    }
+    catch(error){
+        console.log("error")
+    }
+    }
+}
+
+weather.fetchWeather("Denver")
