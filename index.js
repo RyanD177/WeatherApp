14aa401c5627c80df53eebbe3713b9fc6b73dfab
RyanD177 <w0477770@campus.nscc.ca>
2023-05-04 21:52:33 -0300

    weather app practice

--- index.js
+++ index.js
@@ -22,10 +22,17 @@ let weather = {
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
+        const {humidity,temp} = data.main;
+        const {speed} =  data.wind;
+ 
+      
         console.log(name)
         console.log(icon,description)
         document.querySelector(".city").innerHTML = `Weather in ${name}`;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".humidity").innerHTML = `Humidity:  ${humidity}`
+        document.querySelector(".temp").innerHTML = `Temp:  ${temp} °C`
+        document.querySelector(".wind").innerHTML = `Wind Speed:  ${speed} km`
 
 
 
@@ -34,5 +41,5 @@ let weather = {
 }
 
 
-weather.fetchWeather("Zimbabwe")
+weather.fetchWeather("Halifax")
 
